009f6d673c9438334a8abc38b5eebeb9fcd5011b
Throughbit <karthikeyan@throughbit.com>
2024-09-06 16:11:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "white" }}>
+      <header
+        className="App-header"
+        style={{ backgroundColor: "white", color: "black" }}
+      >
+        Coming Soon
+      </header>
+    </div>
+  );
+}
+
+export default App;

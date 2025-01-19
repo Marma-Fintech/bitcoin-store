f74657978f807fd0569d08fec000fe15a7b4359d
ashwinThroughbit <ashwin@throughbit.com>
2025-01-20 00:47:00 +0530

    Initial Commit for Gpotus

--- src/App.js
+++ src/App.js
@@ -1,49 +1,209 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import defaultImage from "../src/assets/goat-p.webp";
+import hoverImage from "../src/assets/trump-hover.webp";
 
-const BitcoinLoading = () => {
+import ticker from "../src/assets/$GPOTUS.svg";
+import telegram from "../src/assets/telegram.png";
+import xlogo from "../src/assets/twitter.png";
+import dexLogo from "../src/assets/dex-screener-logo-png_seeklogo-527276.png"
+import copy from "../src/assets/copy.svg";
+import { FaRegCopy } from "react-icons/fa";
+
+const App = () => {
+  const [copied, setCopied] = useState(false);
+
+  const paragraphText = "HcFcRdPV66y7Vv9dQVD4a4uPQR4mpBHyfzkFGUFfmoon";
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(paragraphText).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset the "copied" state after 2 seconds
+    });
+  };
+  const [isActive, setIsActive] = useState(false); // Tracks if the image is in "hover" or "default" state
+
+  const handleClick = () => {
+    setIsActive((prev) => !prev); // Toggle between active (hoverImage) and defaultImage on tap
+  };
+
+  const handleMouseEnter = () => {
+    setIsActive(true); // Show hover image on mouse enter (desktop)
+  };
+
+  const handleMouseLeave = () => {
+    setIsActive(false); // Revert to default image on mouse leave (desktop)
+  };
+
+  const currentImage = isActive ? hoverImage : defaultImage; // Decide the image to display
+
   return (
-    <div id="bitcoin">
-      <svg
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        width="200px"
-        height="200px"
-        viewBox="100 100 400 400"
-        xmlSpace="preserve"
-      >
-        <filter id="dropshadow" height="130%">
-          <feGaussianBlur in="SourceAlpha" stdDeviation="5" />
-          <feOffset dx="0" dy="0" result="offsetblur" />
-          <feFlood floodColor="red" />
-          <feComposite in2="offsetblur" operator="in" />
-          <feMerge>
-            <feMergeNode />
-            <feMergeNode in="SourceGraphic" />
-          </feMerge>
-        </filter>
-        <path
-          className="path"
-          style={{ filter: "url(#dropshadow)" }}
-          fill="#000000"
-          d="M446.089,261.45c6.135-41.001-25.084-63.033-67.769-77.735l13.844-55.532l-33.801-8.424l-13.48,54.068
-          c-8.896-2.217-18.015-4.304-27.091-6.371l13.568-54.429l-33.776-8.424l-13.861,55.521c-7.354-1.676-14.575-3.328-21.587-5.073
-          l0.034-0.171l-46.617-11.64l-8.993,36.102c0,0,25.08,5.746,24.549,6.105c13.689,3.42,16.159,12.478,15.75,19.658L208.93,357.23
-          c-1.675,4.158-5.925,10.401-15.494,8.031c0.338,0.485-24.579-6.134-24.579-6.134l-9.631,40.468l36.843,9.188
-          c8.178,2.051,16.209,4.19,24.098,6.217l-13.978,56.17l33.764,8.424l13.852-55.571c9.235,2.499,18.186,4.813,26.948,6.995
-          l-13.802,55.309l33.801,8.424l13.994-56.061c57.648,10.902,100.998,6.502,119.237-45.627c14.705-41.979-0.731-66.193-31.06-81.984
-          C425.008,305.984,441.655,291.455,446.089,261.45z M368.859,369.754c-10.455,41.983-81.128,19.285-104.052,13.589l18.562-74.404
-          C306.28,314.65,379.774,325.975,368.859,369.754z M379.302,260.846c-9.527,38.187-68.358,18.781-87.442,14.023l16.828-67.489
-          C327.767,212.14,389.234,221.02,379.302,260.846z"
-        />
-      </svg>
-      <h1>COMING SOON</h1>
-    </div>
+    // <div style={{ position: "relative" }} className="bg-back">
+    //   <div
+    //     className="bg-front"
+    //     style={{
+    //       height: "100vh",
+    //       backgroundColor: "#000",
+    //       display: "flex",
+    //       alignItems: "center",
+    //       justifyContent: "center",
+    //       flexDirection: "column",
+    //     }}
+    //   >
+    //     <h2
+    //       className="heading"
+    //       style={{
+    //         fontSize: "8vw",
+    //         fontWeight: "bolder",
+    //         color: "black",
+    //         letterSpacing: "3px",
+    //         textAlign: "center",
+    //       }}
+    //     >
+    //       <div>The Bitcoin.com</div>
+    //     </h2>
+    //   </div>
+    // </div>
+    <>
+      <div className="bg-image"></div>
+      <div className="row">
+        <div className="bg-text col-11 col-md-10 col-lg-8">
+          <div className="img-flex">
+            <div id="overlay"></div>
+            {/* <img src={gpotus} className="img-goat" draggable="false" /> */}
+            {/* <img
+              src={isHovered ? hoverImage : defaultImage} // Change image on hover
+              className="img-goat"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              alt="Hoverable"
+              style={{
+                position: "absolute",
+                bottom: "0",
+                left: "50%",
+                transformOrigin: "bottom", // Scale from the bottom
+                transform: isHovered
+                  ? "translateX(-50%) scale(1.02)"
+                  : "translateX(-50%) scale(1)",
+                transition: "transform 0.3s ease, opacity 0.3s ease", // Smooth scaling and fading
+              }}
+            /> */}
+            <div
+              style={{
+                // position: "relative",
+                height: "100vh",
+                overflow: "hidden",
+              }}
+            >
+              <img
+                src={currentImage} // Dynamically switch image based on isActive
+                className="img-goat"
+                onMouseEnter={handleMouseEnter} // Hover effect (desktop)
+                onMouseLeave={handleMouseLeave} // Reset hover effect (desktop)
+                onClick={handleClick} // Toggle image on tap (mobile)
+                alt="Hoverable or Toggleable"
+                style={{
+                  position: "absolute",
+                  bottom: "0",
+                  left: "50%",
+                  transformOrigin: "bottom", // Scale from the bottom
+                  transform: isActive
+                    ? "translateX(-50%) scale(1.02)"
+                    : "translateX(-50%) scale(1)",
+                  transition: "transform 0.3s ease, opacity 0.3s ease", // Smooth scaling and fading
+                  cursor: "pointer", // Indicate that the image is clickable
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="content-token">
+            <p className="contract desk">
+              Contract Address : {paragraphText}
+              <button
+                onClick={handleCopy}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                  fontSize: "20px",
+                  color: copied ? "#ef6604" : "white",
+                }}
+                title="Copy to clipboard"
+              >
+                <FaRegCopy />
+              </button>
+            </p>
+            <p className="contract mob">
+              Contract Address :<br /> {`${paragraphText.slice(0, 5)}....${paragraphText.slice(-5)}`}
+              {/* Contract Address :<br /> {paragraphText} */}
+              <button
+                onClick={handleCopy}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                  fontSize: "18px",
+                  color: copied ? "#ef6604" : "white",
+                }}
+                title="Copy to clipboard"
+              >
+                <FaRegCopy />
+              </button>
+            </p>
+            <img src={ticker} className="ticker-coin" />
+            <p className="p-text">Greatest President of the United States</p>
+            <div className="footer">
+              <a href="https://t.me/gpotus">
+                <img src={telegram} className="telegram-bg" draggable="false" />{" "}
+              </a>
+              <a href="https://x.com/gpotus47">
+                <img src={xlogo} className="telegram-bg" draggable="false" />
+              </a>
+              <a href="https://dexscreener.com/solana/8q84o2rzaa1ersgsp1zedfkkiuuzhkzxcsvknkpw67rw">
+                <img src={dexLogo} className="telegram-bg" draggable="false"/>
+                {/* <h4 className="telegram-bg g-text">$GPOTUS</h4> */}
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* <div className="bg-text1 col-10 col-md-7">
+          <p className="contract">Contract Address : 5436754765....78658965 </p>
+          <img src={ticker} className="ticker-coin" />
+          <p className="p-text">Greatest President of the United States</p>
+          <div className="footer">
+            <img src={telegram} className="telegram-bg" draggable="false" />
+            <img src={xlogo} className="telegram-bg" draggable="false" />
+            <h4 className="telegram-bg g-text">$GPOTUS</h4>
+          </div>
+        </div> */}
+        {/* <div className="bot-text">
+          <div class="b-section-marquee-box">
+            <h2 class="marquee-text">
+              $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS
+            </h2>
+            <h2 class="marquee-text">
+              $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS $GPOTUS
+            </h2>
+          </div>
+        </div> */}
+
+        <div className="bot-text">
+          <div className="marquee bot-text">
+            <div className="track">
+              <div className="content">
+                $GPOTUS&nbsp;&nbsp;$GPOTUS&nbsp;&nbsp; $GPOTUS&nbsp;&nbsp;
+                $GPOTUS&nbsp;&nbsp; $GPOTUS&nbsp;&nbsp;$GPOTUS&nbsp;&nbsp;
+                $GPOTUS&nbsp;&nbsp; $GPOTUS&nbsp;&nbsp;
+                $GPOTUS&nbsp;&nbsp;$GPOTUS&nbsp;&nbsp; $GPOTUS&nbsp;&nbsp;
+                $GPOTUS&nbsp;&nbsp;
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default BitcoinLoading;
+export default App;
